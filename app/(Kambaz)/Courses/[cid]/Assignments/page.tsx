e27e98e40c66bb892df941b88cbcb82ddb3a3941
Teja Vejandla <vejandla.03@gmail.com>
2025-09-14 05:50:54 -0400

    Fix deployment build errors

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Assignments() {
   return (
     <div id="wd-assignments">
@@ -9,20 +11,20 @@ export default function Assignments() {
         ASSIGNMENTS 40% of Total <button>+</button> </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A1 - ENV + HTML
-          </a> <br /> Multiple Modules | <b>Not available until</b>  May 6 at 12.00am | <b>Due</b> May 13 at 11:59pm | 100 pts</li>
+          </Link> <br /> Multiple Modules | <b>Not available until</b>  May 6 at 12.00am | <b>Due</b> May 13 at 11:59pm | 100 pts</li>
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A2 - CSS + BOOTSTRAP
-          </a> <br /> Multiple Modules | <b>Not available until</b>  May 13 at 12.00am | <b>Due</b> May 20 at 11:59pm | 100 pts </li>
+          </Link> <br /> Multiple Modules | <b>Not available until</b>  May 13 at 12.00am | <b>Due</b> May 20 at 11:59pm | 100 pts </li>
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A3 - JAVASCRIPT + REACT
-          </a> <br /> Multiple Modules | <b>Not available until</b>  May 20 at 12.00am | <b>Due</b> May 27 at 11:59pm | 100 pts </li>
+          </Link> <br /> Multiple Modules | <b>Not available until</b>  May 20 at 12.00am | <b>Due</b> May 27 at 11:59pm | 100 pts </li>
       </ul>
     </div>
 );}
