3aff5028fd8a6a68e994c16cf441c5728e336b86
Teja Vejandla <vejandla.03@gmail.com>
2025-09-17 23:47:23 -0400

    Add dummy pages for other navigation in course

---
+++ app/(Kambaz)/Courses/[cid]/Quizzes/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default function Quizzes() {
+  return (
+    <div>
+      <h2>Quizzes</h2>
+      <div id="wd-quizzes">
+        <input placeholder="Search for Quizzes"
+              id="wd-search-quiz" />
+        <button id="wd-add-quiz-group">+ Group</button>
+        <button id="wd-add-quiz">+ Quiz</button>
+        <h3 id="wd-quizs-title">
+          QUIZZES 20% of Total <button>+</button> </h3>
+        <ul id="wd-quiz-list">
+          <li className="wd-quiz-list-item">
+            <Link href="/Courses/1234/quizs/123"
+              className="wd-quiz-link" >
+              Q1 - ENV + HTML
+            </Link> <br /> Multiple Modules | <b>Not available until</b>  May 6 at 12.00am | <b>Due</b> May 13 at 11:59pm | 100 pts</li>
+          <li className="wd-quiz-list-item">
+            <Link href="/Courses/1234/Quizzes/123"
+              className="wd-quiz-link" >
+              Q2 - CSS + BOOTSTRAP
+            </Link> <br /> Multiple Modules | <b>Not available until</b>  May 13 at 12.00am | <b>Due</b> May 20 at 11:59pm | 100 pts </li>
+        </ul>
+      </div>
+    </div>
+);}
