2f20db18ec0c93c9960d8fbdb183514cf5f7a531
Teja Vejandla <vejandla.03@gmail.com>
2025-10-29 19:05:16 -0400

    Fix reducer erroe

--- app/Labs/Lab4/ReduxExamples/page.tsx
+++ app/Labs/Lab4/ReduxExamples/page.tsx
@@ -1,17 +1,22 @@
 "use client";
+
+import { Provider } from "react-redux";
+import store from "../store";
 import AddRedux from "./AddRedux";
 import CounterRedux from "./CounterRedux";
 import HelloRedux from "./HelloRedux";
 import TodoList from "./todos/TodoList";
 
 export default function ReduxExamples() {
-  return(
-    <div>
-      <h2>Redux Examples</h2>
-      <HelloRedux/>
-      <CounterRedux/>
-      <AddRedux/>
-      <TodoList/>
-    </div>
+  return (
+    <Provider store={store}>
+      <div>
+        <h2>Redux Examples</h2>
+        <HelloRedux />
+        <CounterRedux />
+        <AddRedux />
+        <TodoList />
+      </div>
+    </Provider>
   );
-};
+}
