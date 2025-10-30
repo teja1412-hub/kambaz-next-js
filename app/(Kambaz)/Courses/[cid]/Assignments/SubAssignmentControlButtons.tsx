e564c4b32ed36c4d7ecc0ffe0769a87358e59cf4
Teja Vejandla <vejandla.03@gmail.com>
2025-10-30 04:13:07 -0400

    Fix the delete alert

--- app/(Kambaz)/Courses/[cid]/Assignments/SubAssignmentControlButtons.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/SubAssignmentControlButtons.tsx
@@ -4,9 +4,15 @@ import {FaTrash } from "react-icons/fa6";
 
 export default function SubAssignmentControlButtons(
   { assignmentId, deleteAssignment}: { assignmentId: string; deleteAssignment: (assignmentId: string) => void;} ) {
+    const handleDelete = () => {
+    const confirmed = window.confirm("Are you sure you want to delete this assignment?");
+    if (confirmed) {
+      deleteAssignment(assignmentId);
+    }
+  };
   return (
     <div className="float-end">
-      <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteAssignment(assignmentId)}/>
+      <FaTrash className="text-danger me-2 mb-1" onClick={handleDelete}/>
       <GreenCheckmark />
       <IoEllipsisVertical className="fs-4" />
     </div> );}
