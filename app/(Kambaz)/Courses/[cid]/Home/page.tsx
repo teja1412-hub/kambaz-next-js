70637af00be7a9886312fb91754935a066fa0b3c
Teja Vejandla <vejandla.03@gmail.com>
2025-11-16 23:54:44 -0500

    Add currentUser check

--- app/(Kambaz)/Courses/[cid]/Home/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/page.tsx
@@ -12,13 +12,13 @@ export default function Home() {
   const params = useParams();
   const courseId = params?.cid as string;
 
-  const { currentUser } = useSelector((state: RootState) => state.accountReducer);
+  const { currentUser } = useSelector((state: RootState) => state.accountReducer) as { currentUser: any };
   const enrollments = useSelector((state: RootState) => state.enrollmentsReducer.enrollments);
 
   // Check if current user is enrolled in this course
   const isEnrolled = enrollments.some(
     (enrollment: any) =>
-      enrollment.user === currentUser._id &&
+      enrollment.user === currentUser?._id &&
       enrollment.course === courseId
   );
 
